c17d0b5fbcf00f3a6d595116a1b22fe02364c2ab
Angelo Junior Diaz Rojas <angelojr13@Angelos-MacBook-Air.local>
2025-09-30 22:08:49 -0500

    c

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -36,7 +36,7 @@ class DocumentController extends Controller
             ],
             'Coordinación Programas' => [ // Nombre corregido para que coincida con el frontend
                 'period' => '2025-II, de septiembre a noviembre',
-                'role_text' => 'bajo el rol de la Coordinación de Proyectos del proyecto _______________________ dentro de la organización Acciones Empáticas.',
+                'role_text' => 'bajo el rol de la Coordinación de Proyectos para el proyecto _______________________ dentro de la organización Acciones Empáticas.',
                 'placeholder_field' => 'project'
             ],
             'Mentores Empáticos' => [
